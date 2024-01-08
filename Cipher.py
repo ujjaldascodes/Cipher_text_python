3bfbf0301b935db553458a2be18a0f9af0f6ade0
Ujjal Das <53267860+ujjaldascodes@users.noreply.github.com>
2024-01-08 22:09:06 +0530

    Add files via upload

---
+++ Cipher.py
@@ -0,0 +1,20 @@
+class Cipher:
+
+    def __init__(self, key=None):
+        if key is None:
+            random.seed(time())
+            key = ''.join(random.choice(ascii_lowercase) for _ in range(100))
+        self.key = key
+
+    def encode(self, text):
+        return ''.join(
+            chr(((ord(character) - 2 * ord('a') + ord(key)) % 26) + ord('a'))
+            for character, key in zip(text, cycle(self.key))
+        )
+
+    def decode(self, text):
+        return ''.join(
+            chr(((ord(character) - ord(key) + 26) % 26) + ord('a'))
+            for character, key in zip(text, cycle(self.key))
+        )
+
